ba36c1c8b024c7ce882df9ba888daabe38ec7e46
Alfaro Esposito, Esteban <ealfesp@gmail.com>
2026-04-21 22:46:15 -0600

    Refactor project structure and Button Component updates
    Restructure components and styling:
    
    - Updating the Button component to use native .css, barrel export its types and definition, handle native HTML props, per variant styles.
    - Adding theme CSS utilities: variables, mixins, functions.
    - Update project dependencies in package.json.

--- src/components/Image/Image.tsx
+++ src/components/Image/Image.tsx
@@ -1,3 +1,8 @@
+export type ImageType = {
+    src: string;
+    alt: string;
+};
+
 const Image: React.FunctionComponent = () => {
     return (
         <img src="" alt="" />
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,16 @@
-import react from "@vitejs/plugin-react";
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+
+import path from 'path';
 import { defineConfig } from "vite";
-import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [react(), tailwindcss()],
+	resolve: {
+		alias: {
+			'@components': path.resolve(__dirname, './src/components'),
+			'@styles': path.resolve(__dirname, './src/theme'),
+		},
+	},
 });
